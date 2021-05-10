5c4eacb167d35c61cdfc95106076337994326b9c
AyoubBF <72936702+AyoubBF@users.noreply.github.com>
2021-05-10 16:06:14 +0200

    Test Coords prototype

---
+++ test/Echiquier/CoordsTest.java
@@ -0,0 +1,23 @@
+package Echiquier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordsTest {
+
+    @Test
+    void getColonne() {
+    }
+
+    @Test
+    void getLigne() {
+    }
+
+    @Test
+    void isNotLegal() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
